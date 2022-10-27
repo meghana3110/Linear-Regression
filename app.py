a68137c6c076003d5dd5c03132e2a35748c56707
Fahina Nihal <fahina.nihal@safrangroup.com>
2022-10-27 12:30:17 +0530

    updated

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 
 import joblib
-import pandas as pd
 import numpy as np
 from flask import Flask, request, jsonify, render_template,Response, send_file
-import matplotlib.pyplot as plt
-
-
-
 
 # Create flask app
 app = Flask(__name__)
 model = joblib.load("clf.pkl")
-v=pd.read_csv("predvsact.csv")
-plt.rcParams["figure.figsize"] = [7.50, 3.50]
-plt.rcParams["figure.autolayout"] = True
 @app.route("/")
 def Home():
     return render_template("website.html")
